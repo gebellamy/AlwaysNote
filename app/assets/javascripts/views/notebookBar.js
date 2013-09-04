090f8c4373e7db86ea71ff03610ce25553ed0c6d
Gabrielle Bellamy <gebellamy@gmail.com>
2013-09-04 10:57:25 -0700

    Search function

--- app/assets/javascripts/views/notebookBar.js
+++ app/assets/javascripts/views/notebookBar.js
@@ -1,3 +1,41 @@
 AlwaysNote.Views.NotebooksBar = Backbone.View.extend({
 	template: JST['notebooks/bar'],
+	
+	events: {
+		"keyup form.search" : "search",
+		"submit form.search" : "doNothing"
+	},
+	
+	render: function() {
+		var content = this.template();
+		this.$el.html(content);
+		return this;
+	},
+	
+	doNothing: function(event) {
+		event.preventDefault();
+	},
+	
+	search: function(event) {
+		if(!AlwaysNote.searchingNotebooks)
+		{
+			AlwaysNote.searchingNotebooks = AlwaysNote.notebooks.clone();
+		}
+		if(event.which !== 16 && event.which !== 13 && event.which !== 27) {
+			AlwaysNote.titleSearch = $(event.currentTarget).serializeJSON()["title"];
+			var searching = AlwaysNote.searchingNotebooks.clone();
+			searching.each(function(notebook) {
+				var title = notebook.escape("title").toLowerCase();
+				if(title.indexOf(AlwaysNote.titleSearch) == -1) {
+					AlwaysNote.searchingNotebooks.remove(notebook);
+				}
+			});
+			AlwaysNote.currentView.notebooks = AlwaysNote.searchingNotebooks;
+			AlwaysNote.currentView.render();
+		} else if(event.which === 27) {
+			AlwaysNote.titleSearch = null;
+			AlwaysNote.currentView.notebooks = AlwaysNote.notebooks;
+			AlwaysNote.currentView.render();
+		}
+	}
 })
